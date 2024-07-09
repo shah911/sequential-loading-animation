28b1f1fc905b8953ddc404453b1d5c5eeb7c3024
shah 911 <mianshahzeb911@gmail.com>
2024-07-09 15:48:28 +0500

    added metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-full h-screen">
-      <h1 className="text-[5vw] tracking-[0.3em] uppercase font-semibold">
-        Loading animation
-      </h1>
-    </main>
+    <>
+      <Head>
+        <title>Loading Animation</title>
+      </Head>
+      <main className="flex items-center justify-center w-full h-screen">
+        <h1 className="text-[5vw] tracking-[0.3em] uppercase font-semibold">
+          Loading animation
+        </h1>
+      </main>
+    </>
   );
 }
